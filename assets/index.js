e0fa00c5676ac3de3c62893eb423e14994853f92
Diana-Petruccetti <dianapetruccetti@gmail.com>
2024-10-01 16:33:00 +0200

    comments

---
+++ assets/index.js
@@ -0,0 +1,21 @@
+console.log('Ciao');
+
+// TRACCIA
+// Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+// FASI COMMENTATE
+// Creare ciclo
+// Creare variabili
+// Creare condizioni
+
+// Fase 1: Preparazione
+
+
+// Fase 2: Raccolta Dati
+
+
+// Fase 3: Elaborazione
+
+
+//Fase 4: Output
