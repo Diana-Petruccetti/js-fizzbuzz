61b50a9557e26fb2f937c5a9cbcd48048d169a90
Diana-Petruccetti <dianapetruccetti@gmail.com>
2024-10-02 10:41:18 +0200

    mistakes fixed

--- assets/index.js
+++ assets/index.js
@@ -18,12 +18,15 @@ console.log('Ciao');
 
 // Fase 3: Elaborazione Dati
 for (let i = 1; i <= 100; i++) {
-    if (i % 15 === 0 ) {
-        console.log('Fizz');
-    } else if (i % 5 === 0 ) {
+ /* if (i % 15 === 0 ) {
+        console.log('FizzBuzz'); 
+    }*/
+    if (i % 5 === 0 && i % 3 === 0) {
+            console.log('FizzBuzz'); 
+    } else if (i % 5 === 0) {
         console.log('Buzz');
-    } else if (i % 3 === 0 ) {
-        console.log('FizzBuzz');
+    } else if (i % 3 === 0) {
+        console.log('Fizz');
     } else {
         console.log(i);
     }
